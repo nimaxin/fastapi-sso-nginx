6546b9806b88466fb12de4b15e4e69e8730d226d
Nima Xin <nazirnasiri786@gmail.com>
2024-11-14 16:56:39 +0330

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+1. clone the repo:
+
+```
+git clone ...
+```
+
+2. create .env file:
+
+```
+CLIENT_ID =
+CLIENT_SECRET =
+```
+
+3. change the domain name and ssl certificate dirs in `nginx.conf`, `compose.yml` and `redirect_uri` if you are not me!
+4. run the project:
+
+```
+docker compose up
+```
---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+
+from dotenv import load_dotenv
+from fastapi import FastAPI, Request
+from fastapi_sso.sso.google import GoogleSSO
+
+load_dotenv()
+
+CLIENT_ID = os.environ["CLIENT_ID"]
+CLIENT_SECRET = os.environ["CLIENT_SECRET"]
+
+app = FastAPI()
+
+sso = GoogleSSO(
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    redirect_uri="https://preprodxin.ddns.net/auth/google/callback",
+    allow_insecure_http=True,
+)
+
+
+@app.get("/auth/login")
+async def auth_init():
+    """Initialize auth and redirect"""
+    async with sso:
+        return await sso.get_login_redirect(
+            params={"prompt": "consent", "access_type": "offline"}
+        )
+
+
+@app.get("/auth/callback")
+async def auth_callback(request: Request):
+    """Verify login"""
+    async with sso:
+        user = await sso.verify_and_process(request)
+    return user
